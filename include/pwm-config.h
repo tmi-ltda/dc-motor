04191044a3f5b52553ab13f4353abd68cde34ad1
Paulo Prates <paulo.prates01@hotmail.com>
2025-05-17 17:06:51 -0300

    V1.0.0: First release

---
+++ include/pwm-config.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define PWM_FREQUENCY 5000
+#define PWM_RESOLUTION 8
---
+++ src/DCMotor.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <pwm-config.h>
+#include <stdint.h>
+#include <Arduino.h>
+
+class DCMotor {
+  private:
+    uint8_t _pin1, _pin2;
+    uint16_t _speed;
+
+  public:
+    DCMotor(uint8_t _pin1, uint8_t _pin2) : _speed(255) {
+      ledcAttach(_pin1, PWM_FREQUENCY, PWM_RESOLUTION);
+      ledcAttach(_pin2, PWM_FREQUENCY, PWM_RESOLUTION);
+
+      stop();
+    }
+
+    void forward() {
+      ledcWrite(_pin1, _speed);
+      ledcWrite(_pin2, 0);
+    }
+
+    void backward() {
+      ledcWrite(_pin1, 0);
+      ledcWrite(_pin2, _speed);
+    }
+
+    void stop() {
+      ledcWrite(_pin1, 0);
+      ledcWrite(_pin2, 0);
+    }
+
+    void setSpeed(uint16_t speed) {
+      _speed = speed;
+    }
+};
